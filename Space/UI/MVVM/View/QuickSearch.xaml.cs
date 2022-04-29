d232f5729f02a4cad43fd0c87e8e7862ae79676f
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2022-04-29 16:30:50 +0300

    progress in search view

--- Space/UI/MVVM/View/QuickSearch.xaml.cs
+++ Space/UI/MVVM/View/QuickSearch.xaml.cs
@@ -41,8 +41,6 @@ namespace UI.MVVM.View
             Future.Text = DateTime.UtcNow.AddDays(30).ToString("MM-dd-yyyy");
             backgroundWorker.DoWork += IntializeListViewAsync;
             backgroundWorker.RunWorkerAsync();
-
-
         }
 
         private void IntializeListViewAsync(object sender, DoWorkEventArgs doWorkEventArgs)
@@ -51,16 +49,7 @@ namespace UI.MVVM.View
 
             foreach (var dic in Listdictionary)
             {
-               
-                    ListAsteroidNames.Add(dic["Name"]);
-              
-
-                //    foreach (var KEYVALUE in dic)  
-                //    {
-                //        if (KEYVALUE.Key = "name") { 
-                //            string name = KEYVALUE.Value;}
-                //    }
-
+                ListAsteroidNames.Add(dic["Name"]);
             }
 
             Dispatcher.Invoke(() =>
@@ -68,11 +57,6 @@ namespace UI.MVVM.View
                 List_asteroids.DataContext = ListAsteroidNames;   
 
             });
-
-
-
-
-            int x = 0;
         }
 
         private void List_asteroids_SelectionChanged(object sender, SelectionChangedEventArgs e)
--- Space/UI/MVVM/View/SearchView.xaml.cs
+++ Space/UI/MVVM/View/SearchView.xaml.cs
@@ -22,10 +22,65 @@ namespace UI.MVVM.View
     {
         string dateNow;
 
+        private const string datePickerHint = "Select initial date";
+        const string datePickerHintEnd = "Select end date";
+
         public SearchView()
         {
             InitializeComponent();
             MonthlyCalendarStart.DisplayDate = DateTime.UtcNow;
+        }
+
+        private void DatePicker_Loaded(object sender, RoutedEventArgs e)
+        {
+            DatePicker datePicker = sender as DatePicker;
+            if (datePicker != null)
+            {
+                System.Windows.Controls.Primitives.DatePickerTextBox datePickerTextBox = FindVisualChild<System.Windows.Controls.Primitives.DatePickerTextBox>(datePicker);
+                if (datePickerTextBox != null)
+                {
+
+                    ContentControl watermark = datePickerTextBox.Template.FindName("PART_Watermark", datePickerTextBox) as ContentControl;
+                    if (watermark != null)
+                    {
+                        watermark.Content = datePickerHint;
+                    }
+                }
+            }
+        }
+
+        private void DatePicker_LoadedEnd(object sender, RoutedEventArgs e)
+        {
+            DatePicker datePicker = sender as DatePicker;
+            if (datePicker != null)
+            {
+                System.Windows.Controls.Primitives.DatePickerTextBox datePickerTextBox = FindVisualChild<System.Windows.Controls.Primitives.DatePickerTextBox>(datePicker);
+                if (datePickerTextBox != null)
+                {
+
+                    ContentControl watermark = datePickerTextBox.Template.FindName("PART_Watermark", datePickerTextBox) as ContentControl;
+                    if (watermark != null)
+                    {
+                        watermark.Content = datePickerHintEnd;
+                    }
+                }
+            }
+        }
+
+        private T FindVisualChild<T>(DependencyObject depencencyObject) where T : DependencyObject
+        {
+            if (depencencyObject != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depencencyObject); ++i)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depencencyObject, i);
+                    T result = (child as T) ?? FindVisualChild<T>(child);
+                    if (result != null)
+                        return result;
+                }
+            }
+
+            return null;
         }
     }
 }
